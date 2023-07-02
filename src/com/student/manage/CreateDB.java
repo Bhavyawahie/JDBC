9f5de6813163215e9fdd547e487c6b475af8b05b
Bhavya Wahie <bhavyawahie@gmail.com>
2023-07-03 02:17:57 +0530

    createDB now returns a connection

--- src/com/student/manage/CreateDB.java
+++ src/com/student/manage/CreateDB.java
@@ -4,39 +4,22 @@ import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-
-import javax.swing.JOptionPane;
 
 public class CreateDB {
     public static Connection connection;
 
-    public static void createDBConnection(String[] args) {
+    public static Connection createDBConnection() {
         connection = null;
         Dotenv dotenv = Dotenv.load();
         String url = dotenv.get("DB_URL");
         String username = dotenv.get("DB_USERNAME");
         String password = dotenv.get("DB_PASSWORD");
-        String dbName = "test";
         try {
             connection = DriverManager.getConnection(url, username, password);
-            Statement statement = connection.createStatement();
-            String sql = "CREATE DATABASE " + dbName;
-            statement.executeUpdate(sql);
-            statement.close();
-            JOptionPane.showMessageDialog(null, dbName + " Database Created Successfully", "Alert", JOptionPane.INFORMATION_MESSAGE);
-            connection.close();
         } catch (SQLException e) {
             System.out.println("Connection failed!");
             e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null && !connection.isClosed()) {
-                    connection.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
+        return connection;
     }
 }
