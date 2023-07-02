c33a475015f5e751c8f0bab993e8d8f32a4a8edb
Bhavya Wahie <bhavyawahie@gmail.com>
2023-07-03 01:04:56 +0530

    student class with constructors and getter/setters created

---
+++ src/Student.java
@@ -0,0 +1,60 @@
+
+
+public class Student {
+    private int studentId;
+    private String studentName;
+    private String studentPhone;
+    private String studentCity;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentPhone() {
+        return studentPhone;
+    }
+
+    public void setStudentPhone(String studentPhone) {
+        this.studentPhone = studentPhone;
+    }
+
+    public String getStudentCity() {
+        return studentCity;
+    }
+
+    public void setStudentCity(String studentCity) {
+        this.studentCity = studentCity;
+    }
+
+    public Student(int studentId, String studentName, String studentPhone, String studentCity) {
+        super();
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.studentPhone = studentPhone;
+        this.studentCity = studentCity;
+    }
+
+    public Student(String studentName, String studentPhone, String studentCity) {
+        super();
+        this.studentName = studentName;
+        this.studentPhone = studentPhone;
+        this.studentCity = studentCity;
+    }
+
+    public Student() {
+        super();
+    }
+
+}
