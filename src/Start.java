cbc365c8490f27959af4ec470b7f4a3dd0d5443e
Bhavya Wahie <bhavyawahie@gmail.com>
2023-07-03 16:05:28 +0530

    updation and getStudentById feature added

--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,10 +10,13 @@ public class Start {
         System.out.println("Welcome to the Student Management application");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
+            System.out.println();
             System.out.println("Press 1 to 'ADD' a new student.");
             System.out.println("Press 2 to 'DELETE' a given student.");
-            System.out.println("Press 3 to 'DISPLAY' a given student.");
-            System.out.println("Press 4 to EXIT");
+            System.out.println("Press 3 to 'UPDATE' a given student.");
+            System.out.println("Press 4 to 'DISPLAY' a given student.");
+            System.out.println("Press 5 to EXIT");
+            System.out.println();
             int choice = Integer.parseInt(bufferedReader.readLine());
             switch (choice) {
                 case 1:
@@ -28,7 +29,7 @@ public class Start {
                     String StudentCity = bufferedReader.readLine();
                     Student student = new Student(StudentName, StudentPhone, StudentCity);
                     if (StudentDAO.addStudentToDB(student)) {
-                        System.out.println("Student added successfully!"); 
+                        System.out.println("Student added successfully!");
                     } else {
                         System.out.println("Something went wrong. Please Try Again!");
                     }
@@ -39,15 +40,50 @@ public class Start {
                     if (StudentDAO.deleteStudentFromDB(StudentID)) {
                         System.out.println("Deleting...");
                         Thread.sleep(500);
-                        System.out.println("Student deleted successfully!"); 
+                        System.out.println("Student deleted successfully!");
                     } else {
                         System.out.println("Something went wrong. Please Try Again!");
                     }
                     break;
                 case 3:
-                    StudentDAO.showAllStudentsFromDB();
+                    System.out.println("Enter The 'ID' of the Student you would want to update.");
+                    int StudentId = Integer.parseInt(bufferedReader.readLine());
+                    Student existingStudent = StudentDAO.getStudentById(StudentId);
+                    if (existingStudent != null) {
+                        System.out
+                                .println("Enter the updated Name of the Student (Leave blank to keep existing value):");
+                        String updatedName = bufferedReader.readLine();
+                        System.out.println(
+                                "Enter the updated Phone Number of the Student (Leave blank to keep existing value):");
+                        String updatedPhone = bufferedReader.readLine();
+                        System.out.println(
+                                "Enter the updated City of Residence of the Student (Leave blank to keep existing value):");
+                        String updatedCity = bufferedReader.readLine();
+                        if (updatedName.isEmpty()) {
+                            updatedName = existingStudent.getStudentName();
+                        }
+                        if (updatedPhone.isEmpty()) {
+                            updatedPhone = existingStudent.getStudentPhone();
+                        }
+                        if (updatedCity.isEmpty()) {
+                            updatedCity = existingStudent.getStudentCity();
+                        }
+                        Student updatedStudent = new Student(updatedName, updatedPhone, updatedCity);
+                        if (StudentDAO.updateStudentInDB(StudentId, updatedStudent)) {
+                            System.out.println("Updating...");
+                            Thread.sleep(500);
+                            System.out.println("Student updated successfully!");
+                        } else {
+                            System.out.println("Something went wrong. Please try again!");
+                        }
+                    } else {
+                        System.out.println("No student found with ID: " + StudentId);
+                    }
                     break;
                 case 4:
+                    StudentDAO.showAllStudentsFromDB();
+                    break;
+                case 5:
                     System.out.println("Exiting...");
                     Thread.sleep(500); // Delay execution for 1 second
                     System.out.println("Adios Amigo!");
--- src/com/student/manage/StudentDAO.java
+++ src/com/student/manage/StudentDAO.java
@@ -7,7 +7,37 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class StudentDAO {
-    
+
+    public static Student getStudentById(int studentId) {
+        Connection connection = CreateDB.createDBConnection();
+        Student student = null;
+        try {
+            String query = "SELECT * FROM STUDENTS WHERE SID=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, studentId);
+            ResultSet set = preparedStatement.executeQuery();
+            if (set.next()) {
+                String name = set.getString("SNAME");
+                String phone = set.getString("SPHONE");
+                String city = set.getString("SCITY");
+                student = new Student(name, phone, city);
+            }
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Oops, Try again!");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return student;
+    }
+
     public static boolean addStudentToDB(Student student) {
         Connection connection = CreateDB.createDBConnection();
         boolean flag = false;
@@ -66,17 +96,17 @@ public class StudentDAO {
             Statement statement = connection.createStatement();
             String query = "SELECT * FROM STUDENTS";
             ResultSet set = statement.executeQuery(query);
-            while(set.next()) {
+            while (set.next()) {
                 int id = set.getInt(1);
                 String name = set.getString(2);
                 String phoneNumber = set.getString(3);
                 String city = set.getString(4);
                 System.out.println();
-                System.out.println("ID: " + id );
-                System.out.println("NAME: " + name );
-                System.out.println("PHONE: " + phoneNumber );
-                System.out.println("CITY: " + city );
-                for(int i = 0; i < 25; i++) {
+                System.out.println("ID: " + id);
+                System.out.println("NAME: " + name);
+                System.out.println("PHONE: " + phoneNumber);
+                System.out.println("CITY: " + city);
+                for (int i = 0; i < 25; i++) {
                     System.out.print("+");
                 }
                 System.out.println();
@@ -93,5 +123,35 @@ public class StudentDAO {
                 e.printStackTrace();
             }
         }
+    }
+
+    public static boolean updateStudentInDB(int studentId, Student updatedStudent) {
+        boolean flag = false;
+        Connection connection = CreateDB.createDBConnection();
+        try {
+            String query = "UPDATE STUDENTS SET SNAME=?, SPHONE=?, SCITY=? WHERE SID=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, updatedStudent.getStudentName());
+            preparedStatement.setString(2, updatedStudent.getStudentPhone());
+            preparedStatement.setString(3, updatedStudent.getStudentCity());
+            preparedStatement.setInt(4, studentId);
+            int rowsEffected = preparedStatement.executeUpdate();
+            if (rowsEffected > 0) {
+                flag = true;
+            }
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Oops, Try again!");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return flag;
     }
 }
